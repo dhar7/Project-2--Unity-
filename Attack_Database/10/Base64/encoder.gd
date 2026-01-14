c8d9661f9f6d16884b52669b3d3dc243af439b57
Argha Dhar <dhar1707069@stud.kuet.ac.bd>
2026-01-14 13:37:25 -0700

    Add files via upload

---
+++ Attack_Database/10/Base64/encoder.gd
@@ -0,0 +1,45 @@
+extends Node
+
+func _ready():
+	print("This image does steganography")
+
+	var image = Image.new()
+	var err = image.load("res://blackboard.jpg")
+	if err != OK:
+		push_error("Could not load blackboard.jpg")
+		return
+
+	# Open the payload file and encode in Base645
+	var file = FileAccess.open("res://calc.exe", FileAccess.READ)
+	var buffer = file.get_buffer(file.get_length())
+	var b64 = Marshalls.raw_to_base64(buffer)
+		
+	var bits = []
+
+	# Convert Base64 to bits + delimiter (0x00)
+	for c in b64.to_utf8_buffer():  # Godot 4.x
+		for i in range(8):
+			bits.append((c >> (7 - i)) & 1)
+	# Add 8 zero bits as end marker
+	for i in range(8):
+		bits.append(0)
+
+	var bit_index = 0
+	for y in range(image.get_height()):
+		for x in range(image.get_width()):
+			if bit_index >= bits.size():
+				break
+			var color = image.get_pixel(x, y)
+			var r = int(color.r * 255.0)
+
+			# Store one bit in the red channel LSB
+			r = (r & ~1) | bits[bit_index]
+			bit_index += 1
+
+			image.set_pixel(x, y, Color8(r, int(color.g * 255.0), int(color.b * 255.0)))
+
+		if bit_index >= bits.size():
+			break
+
+	image.save_png("res://encoded.png")
+	print("Message encoded into encoded.png")
