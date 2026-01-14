3794c5c97c089f70ee5650f7725d489fb3833ee4
Argha Dhar <dhar1707069@stud.kuet.ac.bd>
2026-01-13 22:13:52 -0700

    Delete Attack_Database/A8 directory

--- Attack_Database/A8/Source/SteeringWheelInterface/Private/SteeringWheelInterfaceModule.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Modules/ModuleManager.h"
-
-
-// CRITICAL: This macro is the actual entry point for the module's DLL
-IMPLEMENT_MODULE(FDefaultModuleImpl, SteeringWheelInterface);
